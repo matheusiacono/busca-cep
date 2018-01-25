e13a18775b16695728c4c5dfe7010c7b7b3ef57b
Matheus Iacono <matheus@iacono.com.br>
2018-01-25 16:04:17 -0300

    botao de fechar

---
+++ src/Components/BotaoFechar/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css';
+
+const BotaoFechar = ({ handleClick }) => (
+  <button type="button" className="button-close" onClick={handleClick} />
+);
+
+export default BotaoFechar;
